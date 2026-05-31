5411c2e6129ba872ad536cbda572bb308096e811
YashAgarwalTheWiz <143926886+YashAgarwalTheWiz@users.noreply.github.com>
2026-05-31 14:29:41 +0530

    feat: add PostgreSQL support with docker-compose

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///database.db")
+engine = create_engine("postgresql://yash:1234@localhost:5432/devtracker")
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
